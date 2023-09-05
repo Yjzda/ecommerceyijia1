613472e319892589575a6cf005793c8ca5294f4f
Yijia <zeng.yi.jia@hotmail.com>
2023-09-05 15:28:18 +0200

    good test insomnia ok with app.py

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from model import customer_purchased_item
 import persistance
 app = Flask(__name__)
 
-@app.route("/item-purchase",method='POST')
+@app.route("/item-purchase",methods=['POST'])
 def purchase_item():
     product_name=request.json['product_name']
     quantity=request.json['quantity']
     inventory=persistance.find_inventory()
     customer_purchased_item(product_name,quantity,inventory,persistance)
-    persistance.save_inventory()
+    persistance.save_inventory(inventory)
     return jsonify({"message":"product ajouté au panier"})
+
+if __name__ == "__main__":
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Disable favicon caching
+    app.run(debug=True)
