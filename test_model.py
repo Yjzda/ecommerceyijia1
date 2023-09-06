cada4064f5eb54a716624dd7851773484a775508
Yijia <zeng.yi.jia@hotmail.com>
2023-09-06 10:45:12 +0200

    gestion error

--- test_model.py
+++ test_model.py
@@ -1,29 +1,42 @@
 from model import customer_purchased_item,customer_purchase_multi_items
 import fake_persistance
 def test_custom_purchased_item():
-    inventory={
-        'apple':10,
-        'pear':5
+    inventory = {
+        'apple': 10,
+        'pear': 5
     }
-    #scénario 1
-    product_name="apple"
-    quantity=5
-    error=customer_purchased_item(product_name,quantity,inventory,fake_persistance)
-    fake_persistance.save_inventory(inventory)
-    #injection dépendance
     
-    #si la fonction marche alors inventory.. =5
-    assert inventory['apple']==5
+    # Scenario 1
+    product_name = "apple"
+    quantity = 5
+    error = None
+    
+    try:
+        customer_purchased_item(product_name, quantity, inventory, fake_persistance)
+    except KeyError as e:
+        error = e
+
+    fake_persistance.save_inventory(inventory)
+    
+    # If the function works, inventory['apple'] should be 5
+    assert inventory.get('apple') == 5, "Expected 'apple' quantity to be 5"
     assert error is None
 
-    #scénario 2
+    # Scenario 2
+    product_name = "poulet"
+    quantity = 5
+    error = None
     
-    product_name="poulet"
-    quantity=5
-    error=customer_purchased_item(product_name,quantity,inventory,fake_persistance)
+    try:
+        customer_purchased_item(product_name, quantity, inventory, fake_persistance)
+    except KeyError as e:
+        error = e
+
     fake_persistance.save_inventory(inventory)
-    assert inventory['poulet']==5
-    assert error is not None
+    
+    # Check for KeyError and provide a custom error message
+    assert error is not None, f"Expected KeyError for '{product_name}' but got None"
+
 
 def test_customer_purchased_multi_item():
     #scénario 1
